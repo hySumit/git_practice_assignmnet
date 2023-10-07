54f500cc2793db4a4ca4627c269911dbb2be2753
Sumit Kumar <sumitbth67@gmail.com>
2023-10-07 16:38:00 +0530

    Created code to check prime

---
+++ CheckPrime.js
@@ -0,0 +1,13 @@
+let num = 13;
+count = 0;
+for (let i = 0; i>= num; i++){
+    if(n%i===0){
+        count +=1
+    }
+}
+if (count === 2){
+    console.log("It is a Prime Number")
+}
+else{
+    console.log("It is not a Prime Number")
+}
